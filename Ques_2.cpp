85e62571afdf80824f0559a7a26392f134aa62ef
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-25 21:56:32 +0530

    Ques_2

---
+++ Ques_2.cpp
@@ -0,0 +1,13 @@
+// Write a Program to Read and Print Number Input From the User.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the value : ";
+    cin>>num;
+    cout<<"The value is "<<num;
+    return 0;
+}
