e8ed0d5a675b76a392bb909ca384ed4b56265ccd
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-26 15:17:17 +0530

    Ques_3

---
+++ Ques_3.cpp
@@ -0,0 +1,12 @@
+// Write a Program to Print the ASCII Value of a Character.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<<"Enter the number : ";
+    cin>>c;
+    cout<<"ASCII value of "<<c<<" is "<<int(c);
+    return 0;
+}
