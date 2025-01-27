e5778a6c42179e604a82e2739aa47cabe9183918
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-27 23:03:37 +0530

    Ques_5

--- Ques_5.cpp
+++ Ques_5.cpp
@@ -1,7 +1,17 @@
+// Write a Program to Find the Size of int, float, double, and char.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    cout<<"hello"<<endl;
+    int a = 34;
+    float b = 153.2455;
+    double c = 15476456.3564564;
+    char d = 'c';
+
+    cout<<"Size of int : "<<sizeof(a)<< " bytes"<<endl;
+    cout<<"Size of float : "<<sizeof(b)<< " bytes"<<endl;
+    cout<<"Size of double : "<<sizeof(c)<< " bytes"<<endl;
+    cout<<"Size of char : "<<sizeof(d)<< " bytes"<<endl;
     return 0;
 }
