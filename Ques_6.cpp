38129da0b9f80124f98190c7bb6e4fae80203815
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-28 23:29:31 +0530

    Ques_6

--- Ques_6.cpp
+++ Ques_6.cpp
@@ -1,7 +1,24 @@
+// Write a Program to Find Compound Interest.
+
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
-    cout<<"Hello"<<endl;
+    double principal, rate, time;
+    cout<<"Enter Principal amount : ";
+    cin>>principal;
+    cout<<"Enter rate on interest in % : ";
+    cin>>rate;
+    cout<<"Enter time in year : ";
+    cin>>time;
+
+    rate = rate/100;
+    double Amount = principal * pow((1+rate),time);
+    double CI = Amount - principal;
+    
+    cout<<"Compound Interest is "<<CI;
+
+
     return 0;
 }
