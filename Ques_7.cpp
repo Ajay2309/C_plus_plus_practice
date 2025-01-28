435dbdee4b439cb358015d033c50b6a8e30e14fa
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-28 23:30:57 +0530

    Ques_7

--- Ques_7.cpp
+++ Ques_7.cpp
@@ -1,7 +1,20 @@
+//  Write a Program to Check Even or Odd Integers.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    cout<<"Hello"<<endl;
+    int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+
+    if(num%2 ==0){
+        cout<<"The number is Even "<<endl;
+    }
+    else{
+        cout<<"The number is Odd"<<endl;
+    }
+    
+    
     return 0;
 }
