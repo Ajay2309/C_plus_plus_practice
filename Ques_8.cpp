0400d006470edebe3b1fc3ac698b919eb99a83ef
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-30 22:39:21 +0530

    Ques_8

--- Ques_8.cpp
+++ Ques_8.cpp
@@ -1,7 +1,27 @@
+// Write a Program to Find the Largest Among 3 Numbers.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int a, b, c;
+    cout<<"Enter three number"<<endl;
+    cin>>a>>b>>c;
+    if(a>b){
+        if(a>c){
+            cout<<a<<" is largest number"<<endl;
+        }
+    }
+    if(b>c){
+        if(b>a){
+            cout<<b<<" is largest number"<<endl;
+        }
+    }
+    if(c>b){
+        if(c>a){
+            cout<<c<<" is largest number"<<endl;
+        }
+    }
+
     return 0;
 }
