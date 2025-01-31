a086a907406f672a99645987c821d88bb88cf154
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-01-31 22:30:49 +0530

    Ques_9

--- Ques_9.cpp
+++ Ques_9.cpp
@@ -1,7 +1,17 @@
+// Write a Program to Check if a Given Year Is a Leap Year.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int year;
+    cout<<"Enter the Year : ";
+    cin>>year;
+    if(year%4==0){
+        cout<<year<< " is a leap year";
+    }
+    else{
+        cout<<year<<" is not a leap year";
+    }
     return 0;
 }
