2d0cb0723c5cf59bf7d078e84cc09b29595a9169
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-03 16:54:32 +0530

    ques_8

--- Ques_10.cpp
+++ Ques_10.cpp
@@ -1,7 +1,17 @@
+// Write a Program to Check Whether a Number Is Prime or Not.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int prime;
+    cout<<"Enter the number : ";
+    cin>>prime;
+    if(prime%2==1){
+        cout<<prime<<" is a Prime Number";
+    }
+    else{
+        cout<<prime<<" is not a Prime Number";
+    }
     return 0;
 }
