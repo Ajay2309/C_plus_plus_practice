1deea779be1802236441a3422a7356e5d2431f1d
Ajay Vishwakarma <ajayvishwakarma9711@gmail.com>
2025-02-05 17:19:23 +0530

    Ques_11

--- Ques_11.cpp
+++ Ques_11.cpp
@@ -1,7 +1,17 @@
+// Write a program to reverse a number.
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int n, rev = 0;
+    cout<<"Enter any number : ";
+    cin>>n;
+    while(n!=0){
+        int rem = n%10;
+        rev = rev*10+rem;
+        n = n/10;
+    }
+    cout<<"Reverse of given number is "<<rev<<endl;
     return 0;
 }
